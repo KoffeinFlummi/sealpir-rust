7c033545bb630dc4849ef986f3803121c95a431d
KoffeinFlummi <koffeinflummi@protonmail.com>
2020-12-08 12:23:36 +0100

    Optimize serialization

--- src/lib.rs
+++ src/lib.rs
@@ -3,14 +3,16 @@ extern crate serde;
 #[macro_use]
 extern crate serde_derive;
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 pub struct PirQuery {
+    #[serde(with = "serde_bytes")]
     pub query: Vec<u8>,
     pub num: u32,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 pub struct PirReply {
+    #[serde(with = "serde_bytes")]
     pub reply: Vec<u8>,
     pub num: u32,
 }
